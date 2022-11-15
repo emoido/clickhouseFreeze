2089314aa89dfcfd2b5013bcb52f0a8cc1cc992b
Emrah Idman <45426637+emoido@users.noreply.github.com>
2022-11-15 10:51:42 +0300

    Create insertdatapartitioned.sh

---
+++ insertdatapartitioned.sh
@@ -0,0 +1,2 @@
+
+ls -1 flightlist_*.csv.gz | xargs -P100 -I{} bash -c 'gzip -c -d "{}" | clickhouse-client --password=your_password --date_time_input_format best_effort --query "INSERT INTO sourcedb.sourcetable_partitioned FORMAT CSVWithNames"'
