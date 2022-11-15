28916bf69c21dec1b42848e2f548c88cb6c05c8b
Emrah Idman <45426637+emoido@users.noreply.github.com>
2022-11-15 12:01:07 +0300

    Create 09_desttable_partitioned_local.sql

---
+++ 09_desttable_partitioned_local.sql
@@ -0,0 +1,23 @@
+CREATE TABLE destdb.desttable_partitioned_local
+(
+    `callsign` String,
+    `number` String,
+    `icao24` String,
+    `registration` String,
+    `typecode` String,
+    `origin` String,
+    `destination` String,
+    `firstseen` DateTime,
+    `lastseen` DateTime,
+    `day` DateTime,
+    `latitude_1` Float64,
+    `longitude_1` Float64,
+    `altitude_1` Float64,
+    `latitude_2` Float64,
+    `longitude_2` Float64,
+    `altitude_2` Float64
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMM(day)
+ORDER BY (origin, destination, callsign)
+SETTINGS index_granularity = 8192
